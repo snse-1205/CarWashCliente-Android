b0426f6a461ec790ba0cd5ca6a9d112c3c508ae3
Stacy Sauceda <stacysauceda1200@gmail.com>
2025-04-15 22:06:49 -0600

    Registro vehiculo y Registro ubicacion

---
+++ app/src/main/java/com/example/carwashcliente_android/Activities/RegistroVehiculo.java
@@ -0,0 +1,26 @@
+package com.example.carwashcliente_android.Activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.carwashcliente_android.R;
+
+public class RegistroVehiculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registro_vehiculo);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
