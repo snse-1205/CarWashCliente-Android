2587931ff1584efaefc0641ab4d15cda5ee21e06
IIJaredII <jaredrios2425@gmail.com>
2025-04-15 23:51:01 -0600

    LogOut

--- app/src/main/java/com/example/carwashcliente_android/Activities/PantallaPrincipal.java
+++ app/src/main/java/com/example/carwashcliente_android/Activities/PantallaPrincipal.java
@@ -1,13 +1,18 @@
 package com.example.carwashcliente_android.Activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 
+import com.example.carwashcliente_android.Config.ClientManager;
 import com.example.carwashcliente_android.Fragments.CotizacionFragment;
 
 import com.example.carwashcliente_android.Fragments.HistorialFragment;
@@ -22,7 +27,9 @@ import com.google.firebase.messaging.FirebaseMessaging;
 
 public class PantallaPrincipal extends AppCompatActivity {
 
+    ClientManager clientManager;
     private BottomNavigationView bottomNavigationView;
+    private ImageView logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,10 @@ public class PantallaPrincipal extends AppCompatActivity {
 
         // Configurar Bottom Navigation
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+        logOut = findViewById(R.id.LogoutPantallaPrincipal);
+        clientManager = new ClientManager(this);
+
+        logOut.setOnClickListener(v -> cerrarSesion());
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
 
@@ -46,6 +57,28 @@ public class PantallaPrincipal extends AppCompatActivity {
             return true;
         });
     }
+
+    private void cerrarSesion() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Cerrar sesión");
+        builder.setMessage("¿Estás seguro de que quieres cerrar sesión?");
+
+        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                clientManager.clearClientData();
+                Intent intent = new Intent(PantallaPrincipal.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 
     private void abrirFragment(Fragment fragment, int action) {
         Bundle args = new Bundle();
